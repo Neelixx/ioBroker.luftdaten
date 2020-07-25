4a70ad97fb434c2192714a6fe3ac2ef5b4787d39
Frank Wachtmeister <github@frank-wachtmeister.de>
2020-07-25 18:10:49 +0100

    initial version

---
+++ admin/words.js
@@ -0,0 +1,89 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    "Sensor Type": {
+        "en": "Sensor Type",
+        "de": "Sensorart",
+        "ru": "Тип датчика",
+        "pt": "Tipo de sensor",
+        "nl": "Sensortype",
+        "fr": "Type de capteur",
+        "it": "Tipo di sensore",
+        "es": "Tipo de sensor",
+        "pl": "Typ czujnika",
+        "zh-cn": "传感器类型"
+    },
+    "Local": {
+        "en": "Local",
+        "de": "Lokal",
+        "ru": "Местный",
+        "pt": "Local",
+        "nl": "Lokaal",
+        "fr": "Local",
+        "it": "Locale",
+        "es": "Local",
+        "pl": "Lokalny",
+        "zh-cn": "本地"
+    },
+    "Remote": {
+        "en": "Remote",
+        "de": "Remote",
+        "ru": "Дистанционный пульт",
+        "pt": "Controlo remoto",
+        "nl": "Afgelegen",
+        "fr": "Éloigné",
+        "it": "A distanza",
+        "es": "Remoto",
+        "pl": "Zdalny",
+        "zh-cn": "远程"
+    },
+    "You can find remote sensors here": {
+        "en": "You can find remote sensors <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>here</a>",
+        "de": "<a target='_blank' href='http://deutschland.maps.luftdaten.info/'>Hier</a> findest Du die Sensoren",
+        "ru": "Вы можете найти удаленные датчики <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>здесь</a>",
+        "pt": "Você pode encontrar sensores remotos <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>aqui</a>",
+        "nl": "Remote sensoren vind je <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>hier</a>",
+        "fr": "Vous pouvez trouver des capteurs à distance <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>ici</a>",
+        "it": "Puoi trovare i sensori remoti <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>qui</a>",
+        "es": "Puedes encontrar sensores remotos <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>aquí</a>",
+        "pl": "Możesz znaleźć zdalne czujniki <a target='_blank' href='http://deutschland.maps.luftdaten.info/'>tutaj</a>",
+        "zh-cn": "您可以<a target='_blank' href='http://deutschland.maps.luftdaten.info/'>在这里</a>找到远程传感器"
+    },
+    "Sensor Identifier": {
+        "en": "Sensor Identifier",
+        "de": "Sensorkennung",
+        "ru": "Идентификатор датчика",
+        "pt": "Identificador do sensor",
+        "nl": "Sensoridentificatie",
+        "fr": "Identifiant du capteur",
+        "it": "Identificatore del sensore",
+        "es": "Identificador del sensor",
+        "pl": "Identyfikator czujnika",
+        "zh-cn": "传感器标识符"
+    },
+    "IP (when local) or ID (when remote)": {
+        "en": "IP (when local) or ID (when remote)",
+        "de": "IP (wenn lokal) oder ID (wenn remote)",
+        "ru": "IP (когда локально) или ID (когда удаленно)",
+        "pt": "IP (quando local) ou ID (quando remoto)",
+        "nl": "IP (indien lokaal) of ID (indien op afstand)",
+        "fr": "IP (si local) ou ID (lorsque distant)",
+        "it": "IP (quando locale) o ID (quando remoto)",
+        "es": "IP (cuando es local) o ID (cuando es remoto)",
+        "pl": "IP (gdy lokalny) lub ID (gdy zdalny)",
+        "zh-cn": "IP（本地时）或ID（远程时）"
+    },
+    "Individual name for sensor": {
+        "en": "Individual name for sensor",
+        "de": "Individueller Name für Sensor",
+        "ru": "Индивидуальное имя для датчика",
+        "pt": "Nome individual do sensor",
+        "nl": "Individuele naam voor sensor",
+        "fr": "Nom individuel du capteur",
+        "it": "Nome individuale del sensore",
+        "es": "Nombre individual para sensor",
+        "pl": "Indywidualna nazwa czujnika",
+        "zh-cn": "传感器的个人名称"
+    }
+};
---
+++ main.js
@@ -0,0 +1,349 @@
+/* jshint -W097 */
+/* jshint strict: false */
+/* jslint node: true */
+'use strict';
+
+const utils       = require('@iobroker/adapter-core');
+const request     = require('request');
+const adapterName = require('./package.json').name.split('.').pop();
+
+class Luftdaten extends utils.Adapter {
+
+    constructor(options) {
+        super({
+            ...options,
+            name: adapterName,
+        });
+
+        this.killTimeout = null;
+
+        this.on('ready', this.onReady.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+    }
+
+    async onReady() {
+        const self = this;
+
+        const sensorType = this.config.sensorType;
+        const sensorIdentifier = this.config.sensorIdentifier;
+        const sensorName = (this.config.sensorName === '') ? sensorIdentifier : this.config.sensorName;
+
+        if (sensorIdentifier && sensorName) {
+            const path = (sensorType == 'local') ? sensorIdentifier.replace(/\./g, '_') + '.' : sensorIdentifier + '.';
+
+            const unitList = {
+                P1: 'µg/m³',
+                P2: 'µg/m³',
+                temperature: '°C',
+                humidity: '%',
+                pressure: 'Pa',
+                noise: 'dB(A)',
+                signal: 'dB(A)',
+                min_micro: 'µs',
+                max_micro: 'µs'
+            };
+
+            const roleList = {
+                P1: 'value.ppm',
+                P2: 'value.ppm',
+                temperature: 'value.temperature',
+                humidity: 'value.humidity',
+                pressure: 'value.pressure',
+                noise: 'value',
+                signal: 'value',
+                min_micro: 'value',
+                max_micro: 'value'
+            };
+
+            this.log.debug('sensor type: ' + sensorType + ', sensor identifier: ' + sensorIdentifier + ', sensor name: ' + sensorName);
+
+            this.setObjectNotExists(path + 'name', {
+                type: 'state',
+                common: {
+                    name: 'name',
+                    type: 'string',
+                    role: 'text'
+                },
+                native: {}
+            });
+
+            this.setState(path + 'name', {val: sensorName, ack: true});
+
+            if (sensorType == 'local') {
+                this.log.debug('local request started');
+
+                request(
+                    {
+                        url: 'http://' + sensorIdentifier + '/data.json',
+                        json: true,
+                        time: true,
+                        timeout: 4500
+                    },
+                    (error, response, content) => {
+                        self.log.debug('local request done');
+
+                        if (response) {
+                            self.log.debug('received data (' + response.statusCode + '): ' + JSON.stringify(content));
+
+                            self.setObjectNotExists(path + 'responseCode', {
+                                type: 'state',
+                                common: {
+                                    name: 'responseCode',
+                                    type: 'number',
+                                    role: 'value',
+                                    read: true,
+                                    write: false
+                                },
+                                native: {}
+                            });
+                            self.setState(path + 'responseCode', {val: response.statusCode, ack: true});
+
+                            self.setObjectNotExists(path + 'responseTime', {
+                                type: 'state',
+                                common: {
+                                    name: 'responseTime',
+                                    type: 'number',
+                                    role: 'value',
+                                    unit: 'ms',
+                                    read: true,
+                                    write: false
+                                },
+                                native: {}
+                            });
+                            self.setState(path + 'responseTime', {val: parseInt(response.timingPhases.total), ack: true});
+
+                            if (!error && response.statusCode == 200) {
+                                if (content && Object.prototype.hasOwnProperty.call(content, 'sensordatavalues')) {
+                                    for (const key in content.sensordatavalues) {
+                                        const obj = content.sensordatavalues[key];
+
+                                        let unit = null;
+                                        let role = 'value';
+
+                                        if (obj.value_type.indexOf('SDS_') == 0) {
+                                            unit = 'µg/m³';
+                                            role = 'value.ppm';
+                                        } else if (obj.value_type.indexOf('temperature') >= 0) {
+                                            unit = '°C';
+                                            role = 'value.temperature';
+                                        } else if (obj.value_type.indexOf('humidity') >= 0) {
+                                            unit = '%';
+                                            role = 'value.humidity';
+                                        } else if (obj.value_type.indexOf('pressure') >= 0) {
+                                            unit = 'Pa';
+                                            role = 'value.pressure';
+                                        } else if (obj.value_type.indexOf('noise') >= 0) {
+                                            unit = 'dB(A)';
+                                            role = 'value';
+                                        } else if (Object.prototype.hasOwnProperty.call(unitList, obj.value_type)) {
+                                            unit = unitList[obj.value_type];
+                                            role = roleList[obj.value_type];
+                                        }
+
+                                        self.setObjectNotExists(path + obj.value_type, {
+                                            type: 'state',
+                                            common: {
+                                                name: obj.value_type,
+                                                type: 'number',
+                                                role: role,
+                                                unit: unit,
+                                                read: true,
+                                                write: false
+                                            },
+                                            native: {}
+                                        });
+                                        self.setState(path + obj.value_type, {val: parseFloat(obj.value), ack: true});
+                                    }
+                                } else {
+                                    self.log.warn('Response has no valid content. Check hostname/IP address and try again.');
+                                }
+                            }
+                        } else if (error) {
+                            self.log.warn(error);
+                        }
+                    }
+                );
+            } else if (sensorType == 'remote') {
+                this.log.debug('remote request started');
+
+                request(
+                    {
+                        url: 'https://data.sensor.community/airrohr/v1/sensor/' + sensorIdentifier + '/',
+                        json: true,
+                        time: true,
+                        timeout: 4500
+                    },
+                    (error, response, content) => {
+                        self.log.debug('remote request done');
+
+                        if (response) {
+                            self.log.debug('received data (' + response.statusCode + '): ' + JSON.stringify(content));
+
+                            self.setObjectNotExists(path + 'responseCode', {
+                                type: 'state',
+                                common: {
+                                    name: 'responseCode',
+                                    type: 'number',
+                                    role: 'value',
+                                    read: true,
+                                    write: false
+                                },
+                                native: {}
+                            });
+                            self.setState(path + 'responseCode', {val: response.statusCode, ack: true});
+
+                            self.setObjectNotExists(path + 'responseTime', {
+                                type: 'state',
+                                common: {
+                                    name: 'responseTime',
+                                    type: 'number',
+                                    role: 'value',
+                                    unit: 'ms',
+                                    read: true,
+                                    write: false
+                                },
+                                native: {}
+                            });
+                            self.setState(path + 'responseTime', {val: parseInt(response.timingPhases.total), ack: true});
+
+                            if (!error && response.statusCode == 200) {
+                                if (content && Array.isArray(content) && content.length > 0) {
+                                    const sensorData = content[0];
+
+                                    if (sensorData && Object.prototype.hasOwnProperty.call(sensorData, 'sensordatavalues')) {
+                                        for (const key in sensorData.sensordatavalues) {
+                                            const obj = sensorData.sensordatavalues[key];
+
+                                            let unit = null;
+                                            let role = 'value';
+
+                                            if (Object.prototype.hasOwnProperty.call(unitList, obj.value_type)) {
+                                                unit = unitList[obj.value_type];
+                                                role = roleList[obj.value_type];
+                                            }
+
+                                            self.setObjectNotExists(path + 'SDS_' + obj.value_type, {
+                                                type: 'state',
+                                                common: {
+                                                    name: obj.value_type,
+                                                    type: 'number',
+                                                    role: role,
+                                                    unit: unit,
+                                                    read: true,
+                                                    write: false
+                                                },
+                                                native: {}
+                                            });
+                                            self.setState(path + 'SDS_' + obj.value_type, {val: parseFloat(obj.value), ack: true});
+                                        }
+                                    } else {
+                                        self.log.warn('Response has no valid content. Check hostname/IP address and try again.');
+                                    }
+
+                                    if (Object.prototype.hasOwnProperty.call(sensorData, 'location')) {
+                                        self.setObjectNotExists(path + 'location', {
+                                            type: 'channel',
+                                            common: {
+                                                name: 'Location',
+                                                role: 'value.gps'
+                                            },
+                                            native: {}
+                                        });
+
+                                        self.setObjectNotExists(path + 'location.longitude', {
+                                            type: 'state',
+                                            common: {
+                                                name: 'Longtitude',
+                                                type: 'number',
+                                                role: 'value.gps.longitude',
+                                                unit: '°',
+                                                read: true,
+                                                write: false
+                                            },
+                                            native: {}
+                                        });
+                                        self.setState(path + 'location.longitude', {val: sensorData.location.longitude, ack: true});
+
+                                        self.setObjectNotExists(path + 'location.latitude', {
+                                            type: 'state',
+                                            common: {
+                                                name: 'Latitude',
+                                                type: 'number',
+                                                role: 'value.gps.latitude',
+                                                unit: '°',
+                                                read: true,
+                                                write: false
+                                            },
+                                            native: {}
+                                        });
+                                        self.setState(path + 'location.latitude', {val: sensorData.location.latitude, ack: true});
+
+                                        self.setObjectNotExists(path + 'location.altitude', {
+                                            type: 'state',
+                                            common: {
+                                                name: 'Altitude',
+                                                type: 'number',
+                                                role: 'value.gps.elevation',
+                                                unit: 'm',
+                                                read: true,
+                                                write: false
+                                            },
+                                            native: {}
+                                        });
+                                        self.setState(path + 'location.altitude', {val: sensorData.location.altitude, ack: true});
+
+                                        self.setObjectNotExists(path + 'timestamp', {
+                                            type: 'state',
+                                            common: {
+                                                name: 'Last Update',
+                                                type: 'string',
+                                                role: 'date',
+                                                read: true,
+                                                write: false
+                                            },
+                                            native: {}
+                                        });
+                                        self.setState(path + 'timestamp', {val: sensorData.timestamp, ack: true});
+                                    }
+                                }
+                            }
+                        } else if (error) {
+                            self.log.warn(error);
+                        }
+                    }
+                );
+            }
+        } else {
+            this.log.debug('sensor type and/or sensor identifier not defined');
+        }
+
+        this.killTimeout = setTimeout(this.stop.bind(this), 10000);
+    }
+
+    onUnload(callback) {
+        try {
+
+            if (this.killTimeout) {
+                this.log.debug('clearing kill timeout');
+                clearTimeout(this.killTimeout);
+            }
+
+            this.log.debug('cleaned everything up...');
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Luftdaten(options);
+} else {
+    // otherwise start the instance directly
+    new Luftdaten();
+}
